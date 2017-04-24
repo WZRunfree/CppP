b29e16d4f88f42e1e6aa4e4e129e03eab8d583f8
WZRunfree <wangzirun93@hotmail.com>
2017-04-24 16:36:09 +0800

    modify accumulateEqual.cpp

--- 10/accumulateEqual.cpp
+++ 10/accumulateEqual.cpp
@@ -8,6 +8,8 @@ int main()
     // accumulate
     cout << accumulate(vec.begin(), vec.end(), 0) << endl;
     // equal
+    // 要求第三个参数，即第二个容器的迭代器后序列 至少等于 前面表示范围的大小，否则访问超出容器，出现未定义错误！
+    // 操作两个容器迭代器的大多数泛型算法都有此要求
     vector<int> vec1(vec.size(), 1);
     cout << equal(vec.cbegin(), vec.cend(), vec1.begin()) << endl;
 
